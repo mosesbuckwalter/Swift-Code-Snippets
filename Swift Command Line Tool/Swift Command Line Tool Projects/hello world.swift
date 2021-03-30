1453642e1e9f3add0f04d5c5d359f03ce37bdd7c
Moses Buckwalter <mosesbuckwalter@users.com>
2021-03-30 08:05:27 -0400

    Update files and main.swift

--- Swift Command Line Tool/Swift Command Line Tool Projects/hello world.swift
+++ Swift Command Line Tool/Swift Command Line Tool Projects/hello world.swift
@@ -1,9 +1,11 @@
 //
-//  name.swift
+//  main.swift
 //  Swift Command Line Tool
 //
 //  Created by Moses Buckwalter on 3/29/21.
 //
 
 import Foundation
-
+func hello(){
+print("Hello World")
+}
--- Swift Command Line Tool/Swift Command Line Tool/main.swift
+++ Swift Command Line Tool/Swift Command Line Tool/main.swift
@@ -1,11 +1,16 @@
 //
-//  main.swift
+//  name.swift
 //  Swift Command Line Tool
 //
 //  Created by Moses Buckwalter on 3/29/21.
 //
 
 import Foundation
+func name() -> Void {
+    print("What is your name?")
+    if let str = readLine(){
+        print("Hello \(str)")
+    }
+}
 
-print("Hello, World!")
-
+name()
