f45d42db4cc88a5974bb9a4caebdbb94ec8f84ed
Moses Buckwalter <mosesbuckwalter@users.com>
2021-04-13 07:49:35 -0400

    Write to file broken

--- Swift Command Line Tool/Swift Command Line Tool/main.swift
+++ Swift Command Line Tool/Swift Command Line Tool/main.swift
@@ -11,29 +11,73 @@ func input() -> String {
     let value = (input)
     return(value ?? "EXIT")
 }
-func read() {
-    // Set the file path
-    let path = "/Users/mosesbuckwalter/Developer/Xcode/Swift-Code-Snippets/Swift Command Line Tool/Swift Command Line Tool/File"
-
-    do {
-        // Get the contents
-        let contents = try String(contentsOfFile: path, encoding: .utf8)
-        let contentsArr = contents.components(separatedBy: " ")
-        var list = contentsArr
-        
-        let reversed2 = list.sorted { $0 > $1 }
-        print(contentsArr)
-        print(list)
-        print(reversed2)
-        
+func write() {
+    let fileName = "File"
+      var filePath = ""
+      
+      // Fine documents directory on device
+      let dirs : [String] = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.allDomainsMask, true)
+      
+      if dirs.count > 0 {
+          let dir = dirs[0] //documents directory
+          filePath = dir.appending("/" + fileName)
+          print("Local path = \(filePath)")
+      } else {
+          print("Could not find local directory to store file")
+          return
+      }
+     
+    while input() != "EXIT" {
+        // Set the contents
+        let fileContentToWrite = String(input())
+      
+        do {
+            // Write contents to file
+            try fileContentToWrite.write(toFile: filePath, atomically: false, encoding: String.Encoding.utf8)
+        }
+        catch let error as NSError {
+            print("An error took place: \(error)")
+        }
     }
-    catch let error as NSError {
-        print("Ooops! Something went wrong: \(error)")
+
+      // Read file content. Example in Swift
+      do {
+          // Read file content
+          let contentFromFile = try NSString(contentsOfFile: filePath, encoding: String.Encoding.utf8.rawValue)
+          print(contentFromFile)
+      }
+      catch let error as NSError {
+          print("An error took place: \(error)")
+      }
+}
+func add() {
+    class Logger {
+
+        static var logFile: URL? {
+            guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+           
+            let fileName = "file.txt"
+            return documentsDirectory.appendingPathComponent(fileName)
+        }
+
+        static func log(_ message: String) {
+            guard let logFile = logFile else {
+                return
+            }
+
+            let data = input()
+            
+
+            if FileManager.default.fileExists(atPath: logFile.path) {
+                if let fileHandle = try? FileHandle(forWritingTo: logFile) {
+                    fileHandle.seekToEndOfFile()
+                    fileHandle.write(data)
+                    fileHandle.closeFile()
+                }
+            } else {
+                try? data.write(to: logFile, options: .atomicWrite)
+            }
+        }
     }
 }
-read()
-
-while input() != "EXIT" {
-    input()
-    
-}
+add()
