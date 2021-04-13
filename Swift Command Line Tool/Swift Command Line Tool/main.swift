52d51aabd2d816ac121628ef649a2b9554cb5738
Moses Buckwalter <mosesbuckwalter@users.com>
2021-04-13 08:17:21 -0400

    also broken

--- Swift Command Line Tool/Swift Command Line Tool/main.swift
+++ Swift Command Line Tool/Swift Command Line Tool/main.swift
@@ -11,73 +11,39 @@ func input() -> String {
     let value = (input)
     return(value ?? "EXIT")
 }
-func write() {
-    let fileName = "File"
-      var filePath = ""
-      
-      // Fine documents directory on device
-      let dirs : [String] = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.allDomainsMask, true)
-      
-      if dirs.count > 0 {
-          let dir = dirs[0] //documents directory
-          filePath = dir.appending("/" + fileName)
-          print("Local path = \(filePath)")
-      } else {
-          print("Could not find local directory to store file")
-          return
-      }
-     
-    while input() != "EXIT" {
-        // Set the contents
-        let fileContentToWrite = String(input())
-      
-        do {
-            // Write contents to file
-            try fileContentToWrite.write(toFile: filePath, atomically: false, encoding: String.Encoding.utf8)
-        }
-        catch let error as NSError {
-            print("An error took place: \(error)")
-        }
-    }
+import Foundation
 
-      // Read file content. Example in Swift
-      do {
-          // Read file content
-          let contentFromFile = try NSString(contentsOfFile: filePath, encoding: String.Encoding.utf8.rawValue)
-          print(contentFromFile)
-      }
-      catch let error as NSError {
-          print("An error took place: \(error)")
-      }
+enum FileWriteError: Error {
+    case directoryDoesntExist
+    case convertToDataIssue
 }
-func add() {
-    class Logger {
 
-        static var logFile: URL? {
-            guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
-           
-            let fileName = "file.txt"
-            return documentsDirectory.appendingPathComponent(fileName)
+protocol FileWriter {
+    var fileName: String { get }
+    func write(_ text: String) throws
+}
+
+extension FileWriter {
+    var fileName: String { return "File.txt" }
+
+    func write(_ text: String) throws {
+        guard let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            throw FileWriteError.directoryDoesntExist
         }
 
-        static func log(_ message: String) {
-            guard let logFile = logFile else {
-                return
-            }
+        let encoding = String.Encoding.utf8
 
-            let data = input()
-            
+        guard let data = text.data(using: encoding) else {
+            throw FileWriteError.convertToDataIssue
+        }
 
-            if FileManager.default.fileExists(atPath: logFile.path) {
-                if let fileHandle = try? FileHandle(forWritingTo: logFile) {
-                    fileHandle.seekToEndOfFile()
-                    fileHandle.write(data)
-                    fileHandle.closeFile()
-                }
-            } else {
-                try? data.write(to: logFile, options: .atomicWrite)
-            }
+        let fileUrl = dir.appendingPathComponent(fileName)
+
+        if let fileHandle = FileHandle(forWritingAtPath: fileUrl.path) {
+            fileHandle.seekToEndOfFile()
+            fileHandle.write(data)
+        } else {
+            try text.write(to: fileUrl, atomically: false, encoding: encoding)
         }
     }
 }
-add()
