288927f56e076d0824b6874bf306306c0a26c20e
Moses Buckwalter <mosesbuckwalter@users.com>
2021-04-14 10:48:03 -0400

    Write to text maybe

--- Swift Command Line Tool/Swift Command Line Tool/main.swift
+++ Swift Command Line Tool/Swift Command Line Tool/main.swift
@@ -5,51 +5,26 @@
 //  Created by Moses Buckwalter on 3/29/21.
 //
 import Foundation
-func intArray() -> Array <Int> {
-    let path = "/Users/mosesbuckwalter/Developer/Xcode/Swift-Code-Snippets/Swift Command Line Tool/Swift Command Line Tool/File"
-    do {
-            let numbers = try String(contentsOfFile: path).components(separatedBy: " ")
-                .compactMap {Int($0)}
-            return numbers
-        } catch {
-            return [Int]()
-        }
+func input() -> String {
+    let input = readLine()
+    let value = (input)
+    return(value ?? "EXIT")
 }
-extension Array where Element: Comparable {
-func selectionSort() -> Array<Element> {
-        
-        //check for trivial case
-        guard self.count > 1 else {
-            return self
-        }
-        
-        //mutated copy
-        var output: Array<Element> = self
-                
-        for primaryindex in 0..<output.count {
-                        
-            var minimum = primaryindex
-            var secondaryindex = primaryindex + 1
-                        
-            while secondaryindex < output.count {
-         
-          //store lowest value as minimum
-                if output[minimum] > output[secondaryindex] {
-                    minimum = secondaryindex
-                }
-                secondaryindex += 1
-            }
-            
-            
-            //swap minimum value with array iteration
-            if primaryindex != minimum {
-            output.swapAt(primaryindex, minimum)
-            }
-        }
-                
-        return output
+func write() {
+    let filePath = "/Users/mosesbuckwalter/Developer/Xcode/Swift-Code-Snippets/Swift Command Line Tool/Swift Command Line Tool/File"
+    var arrayInput: [String] = []
+    while input() != "EXIT" {
+        arrayInput.append(input())
+        input()
+        print(arrayInput)
+    }
+    let joinedStrings = arrayInput.joined(separator: "\n")
+
+    do {
+        try joinedStrings.write(toFile: filePath, atomically: true, encoding: .utf8)
+    } catch let error {
+        // handle error
+        print("Error on writing strings to file: \(error)")
     }
 }
-let results: Array<Int> = intArray().selectionSort()
-print(results)
-
+write()
