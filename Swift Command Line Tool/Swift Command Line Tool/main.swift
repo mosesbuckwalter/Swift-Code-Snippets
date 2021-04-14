5b78f5ba08846d81f1b997972e39d7ab453d6ce9
Moses Buckwalter <mosesbuckwalter@users.com>
2021-04-13 21:58:31 -0400

    Sort done

--- Swift Command Line Tool/Swift Command Line Tool/main.swift
+++ Swift Command Line Tool/Swift Command Line Tool/main.swift
@@ -6,7 +6,6 @@
 //
 import Foundation
 func intArray() -> Array <Int> {
-    // Set the file path
     let path = "/Users/mosesbuckwalter/Developer/Xcode/Swift-Code-Snippets/Swift Command Line Tool/Swift Command Line Tool/File"
     do {
             let numbers = try String(contentsOfFile: path).components(separatedBy: " ")
